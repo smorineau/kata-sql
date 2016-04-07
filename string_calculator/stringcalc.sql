ddbcb8951cefcf573aacc5730d21f82ce037c621
seb-zenika <sebastien.morineau@zenika.com>
2016-04-07 18:01:45 +0200

    String Calculator - Handles separators "," and "n"

--- string_calculator/stringcalc.sql
+++ string_calculator/stringcalc.sql
@@ -9,11 +9,39 @@ show err
 create or replace package body stringcalc
 is
    type integer_list is table of pls_integer;
+   type separator_list is table of varchar2(1);
    function parse_string(string in varchar2) return integer_list;
    function add_terms(terms in integer_list) return pls_integer;
 
+   function get_separators return separator_list is
+      separators    separator_list;
+   begin
+      separators:=separator_list(',','n');
+      return separators;
+   end;
+
+   function get_next_separator_position(string in varchar2, start_position in pls_integer) return pls_integer is
+      next_separator_position   pls_integer := length(string);
+      separator_position        pls_integer := 0;
+      sum_of_positions          pls_integer := 0;
+      separators                separator_list:=separator_list();
+   begin
+      separators := get_separators;
+      for i in separators.FIRST..separators.LAST
+      loop
+         separator_position := instr(string, separators(i), start_position);
+         if separator_position <> 0 and separator_position < next_separator_position then next_separator_position := separator_position; end if;
+         sum_of_positions := sum_of_positions + separator_position;
+      end loop;
+      if sum_of_positions = 0 then
+         return 0;
+      else
+         return next_separator_position;
+      end if;
+   end;
+
    function parse_string(string in varchar2) return integer_list is
-      
+
       terms             integer_list := integer_list();
       start_position    pls_integer := 1;
       next_separator_position   pls_integer;
@@ -21,19 +49,19 @@ is
 
       while true
       loop
-         next_separator_position := instr(string, ',', start_position);
+         next_separator_position := get_next_separator_position(string, start_position);
          exit when next_separator_position = 0;
 
          terms.extend;
-         terms(terms.LAST) := substr(string, start_position, next_separator_position - start_position);   
+         terms(terms.LAST) := substr(string, start_position, next_separator_position - start_position);
          start_position := next_separator_position + 1;
       end loop;
-   
+
       terms.extend;
       terms(terms.LAST) := substr(string, start_position, length(string) - start_position + 1);
 
       return terms;
-   
+
    end;
 
 
--- string_calculator/ut_stringcalc.sql
+++ string_calculator/ut_stringcalc.sql
@@ -31,7 +31,16 @@ is
          stringcalc.add('1'),
          1);
       utassert.eq('Should return 3 when given "1,2"',
-         stringcalc.add('1,2'),
+         stringcalc.add(q'[1,2]'),
+         3);
+      utassert.eq('Should return 6 when given "1,2,3"',
+         stringcalc.add(q'[1,2,3]'),
+         6);
+      utassert.eq('Should return 6 when given "1,2n3"',
+         stringcalc.add(q'[1,2n3]'),
+         6);
+      utassert.eq('Should return 3 when given "1n2"',
+         stringcalc.add('1n2'),
          3);
       utassert.eq('Should return 0 when given ""',
          stringcalc.add(''),
